3cf7367e098f319d0591f637c95267c1dd7597cb
KZLenin <leninmateo4@gmail.com>
2025-10-20 02:30:14 -0500

    fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { PhotoService } from '../services/photo';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: false,
 })
 export class HomePage {
   constructor(public photoService: PhotoService) {}
